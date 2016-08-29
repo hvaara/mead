9b83348a58a25cbad94499a7340d38c796d576d2
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-08-29 20:58:00 +0200

    Move plugins out of main repo

--- src/config/baseConfig.js
+++ src/config/baseConfig.js
@@ -1,8 +1,7 @@
-const fsSource = require('../plugins/mead-plugin-source-fs')
-const s3Source = require('../plugins/mead-plugin-source-s3')
-const proxySource = require('../plugins/mead-plugin-source-proxy')
-const webfolderSource = require('../plugins/mead-plugin-source-webfolder')
-const configSourceResolver = require('../plugins/mead-plugin-source-resolver-config')
+const fsSource = require('mead-plugin-source-fs')
+const proxySource = require('mead-plugin-source-proxy')
+const webfolderSource = require('mead-plugin-source-webfolder')
+const configSourceResolver = require('mead-plugin-source-resolver-config')
 
 module.exports = {
   sources: [],
@@ -11,7 +10,6 @@ module.exports = {
   plugins: [
     configSourceResolver,
     fsSource,
-    s3Source,
     proxySource,
     webfolderSource
   ]
