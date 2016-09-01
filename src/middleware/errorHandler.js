2c589f9aff23788081dec560f203e154876ccf32
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-09-01 17:35:03 +0200

    Don't log errors to console, pass errors on to next handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -5,9 +5,10 @@ const Boom = require('boom')
 module.exports = (err, req, res, next) => {
   const error = err.isBoom ? err : Boom.wrap(err)
 
-  // If it's a 5xx we want to log it
+  // If it's a 5xx we want to log it, and pass it on to any other error handlers
   if (error.isServer) {
-    console.error(error)
+    next(error)
+    return
   }
 
   res.status(error.output.statusCode).send(error.output.payload)
