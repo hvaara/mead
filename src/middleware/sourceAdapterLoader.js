3fb936de5b52ffc29a5865a396260a95e80c20b0
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-09-03 21:07:17 +0200

    Alter the expected source adapter config format

--- src/middleware/sourceAdapterLoader.js
+++ src/middleware/sourceAdapterLoader.js
@@ -2,12 +2,12 @@ const Boom = require('boom')
 
 module.exports = (req, res, next) => {
   const source = res.locals.source
-  const adapter = req.app.locals.plugins.source[source.adapter]
+  const adapter = req.app.locals.plugins.source[source.adapter.type]
   if (!adapter) {
-    next(Boom.notImplemented(`Adapter type "${source.adapter}" not implemented`))
+    next(Boom.notImplemented(`Adapter type "${source.adapter.type}" not implemented`))
     return
   }
 
-  res.locals.sourceAdapter = adapter(source.config || {})
+  res.locals.sourceAdapter = adapter(source.adapter.config || {})
   next()
 }
